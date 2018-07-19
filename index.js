fd15d2ee9d5a0347f1a15f96240f4280b8c74405
josh.ray.carr@gmail.com <josh.ray.carr@gmail.com>
2018-07-19 02:20:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,6 @@ function sayHiToGrandma(string) {
     return(`I can\'t hear you!`)
     if (string===uppercase) 
     return('YES INDEED!')
-    if (string===`)
+    if (string===`I love you, Grandma.`)
+    return('I love you, too.')
   }
